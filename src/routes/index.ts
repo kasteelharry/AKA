cace65a220cbc1adbc9b302ec2afea5d8ba90339
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-25 17:07:26 +0100

    Added index file fix routing problem by bad commit

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+const router = express.Router()
+// define a route handler for the default home page
+app.get('/', (req, res, next) => {
+    res.render('login')
+  })
+export default app
