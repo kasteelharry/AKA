d850f42d9bbf8c199a86eaf6207ed6b77c40707e
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-25 17:05:18 +0100

    Added mysql database connection

---
+++ src/database/database.ts
@@ -0,0 +1,20 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const hostname = process.env.DATABASE_HOST
+const database = process.env.DATABASE_SCHEMA
+const password = process.env.DATABASE_PASSWORD
+const username = process.env.DATABASE_USER
+export const db = mysql.createConnection({
+    host: hostname, 
+    user:username, 
+    password: password,
+    database: database,
+    port: 3306,
+    timezone: 'Europe/Amsterdam'
+});
+
+console.log(`connecting to database ${database} on ${hostname}`)
+
