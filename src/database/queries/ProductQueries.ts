443673efa45c08e86b6b936860b3a5719a0bf395
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-26 15:27:57 +0100

    Added individual product query endpoints

--- src/database/queries/ProductQueries.ts
+++ src/database/queries/ProductQueries.ts
@@ -1,4 +1,6 @@
 import { RowDataPacket } from "mysql2";
+import { UnexpectedSQLResultError } from "../../exceptions/UnexpectedSQLResultError";
+import { EmptySQLResultError } from "../../exceptions/EmptySQLResultError";
 import { BasicProduct, Product } from "../../model/Products";
 import { db } from "../Database";
 
@@ -18,5 +20,42 @@ export const getAllProducts = (callback: Function) => {
             resultArr.push(product);
         });
         callback(null, resultArr);
+    });
+}
+
+export const getProductByID = (productID:string, callback: Function) => {
+    const queryOne = "SELECT * FROM ak_products p WHERE p.id = ?;";
+    const queryTwo = "SELECT * FROM ak_products p WHERE p.name LIKE ?";
+    let query = "";
+    const numberID = parseInt(productID);
+    if (isNaN(numberID)) {
+        query = queryTwo;
+        productID = '%' + productID + '%';
+    } else {
+        query = queryOne
+    }
+    db.query(query, productID, (err, result) => {
+        try {
+            if (err) {
+                callback(err);
+            }
+            const rows = <RowDataPacket[]> result;
+            if (rows == undefined) {
+                throw new EmptySQLResultError("No match found.");
+            } else if (rows.length == 1) {
+                rows.forEach(row => {
+                    const product: Product = {
+                        id: row.ID,
+                        name: row.Name,
+                        archived: row.Archived
+                    }
+                    callback(null, product);
+                });
+            } else {
+                throw new UnexpectedSQLResultError("Expected 1 result, received " + rows.length);
+            }
+        } catch (error) {
+            callback(error);
+        }
     });
 }
--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction } from 'express';
-import { getAllProducts } from '../database/queries/productQueries';
+import { getAllProducts, getProductByID } from '../database/queries/productQueries';
 import { BasicProduct, Product } from "../model/Products";
 
 const router = express.Router();
@@ -11,6 +11,18 @@ router.get('/', (req, res, next) => {
         } else {
             console.log(product);
             res.status(200).json({"products:":product})
+        }
+    });
+});
+
+router.get('/:productID', (req, res, next) => {
+    const productID = req.params.productID;
+    getProductByID(productID, (err:Error, product:Product) => {
+        if (err) {
+            next(err)
+        } else {
+            console.log(product);
+            res.status(200).json({"product:":product});
         }
     });
 });
