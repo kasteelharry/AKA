afea84e45fa9e3b37b731b01db42fefbfb59c99e
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-29 15:45:47 +0100

    Added improved fucntion for chained queries

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,4 @@
-import mysql, { PoolConnection } from 'mysql2';
+import mysql, { FieldPacket, PoolConnection } from 'mysql2';
 import dotenv from 'dotenv';
 import { ItemAlreadyExistsError } from '../exceptions/ItemAlreadyExistsError';
 import { UnexpectedSQLResultError } from '../exceptions/UnexpectedSQLResultError';
@@ -18,6 +18,58 @@ export const db = mysql.createPool({
     port: 3306,
     timezone: 'Europe/Amsterdam'
 });
+
+
+export const executeTransactions = async (queries: Array<{ id: number, query: string, parameters: Array<string | number | boolean | JSON> }>): Promise<{ [id: string]: any }> => {
+    return new Promise(async (resolve, reject) => {
+        const results: {[id: string]: {result: any, fields: FieldPacket[] | undefined} }= {};
+        db.getConnection((err, connection) => {
+            try {
+                connection.on("error", (error: Error) => {
+                    if (error.message.match('Duplicate entry')) {
+                        reject(new ItemAlreadyExistsError())
+                    } else {
+                        reject(new UnexpectedSQLResultError("Something went wrong with the connection."))
+                    }
+                    return
+                })
+            } catch (error) {
+                reject(new UnexpectedSQLResultError("Something went wrong with the database."));
+                return;
+            }
+            for (const query of queries) {
+                connection.query(query.query, query.parameters, (err, queryResult, fields) => {
+                    // If the query errored, then rollback and reject
+                    if (err !== null) {
+                        // Try catch the rollback end reject if the rollback fails
+                        try {
+                            connection.rollback(() => {
+                                reject(new UnexpectedSQLResultError("Something went wrong with query " + query.id + "."));
+                            });
+                        } catch (rollbackError) {
+                            reject(new UnexpectedSQLResultError("Failed to roll back."));
+                        }
+                    }
+                    
+                    // Push the result into an array and index it with the ID passed for searching later
+                    results[query.id] = {
+                        result: queryResult,
+                        fields: fields,
+                    };
+                });
+            }
+            connection.commit((commitError) => {
+                if (commitError !== null) {
+                    reject(new UnexpectedSQLResultError("Something went wrong with the query."));
+                }
+                console.log("results have been saved");
+                
+                resolve(results);
+            });
+        });
+    });
+}
+
 
 export const executePreparedQuery = (query: string, callback: Function, param?: any) => {
     db.getConnection((err, connection) => {
