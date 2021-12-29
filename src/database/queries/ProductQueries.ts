8e50c3e78bae6c04965364a00f6c2464463e4f09
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-29 16:33:35 +0100

    Added endpoints for deletion and archival

--- src/database/queries/ProductQueries.ts
+++ src/database/queries/ProductQueries.ts
@@ -1,5 +1,8 @@
 
-import { executePreparedQuery } from "../Database";
+import { FieldPacket } from "mysql2";
+import { EmptySQLResultError } from "../../exceptions/EmptySQLResultError";
+import { ItemAlreadyExistsError } from "../../exceptions/ItemAlreadyExistsError";
+import { executePreparedQuery, executeTransactions } from "../Database";
 
 
 // 
@@ -24,9 +27,27 @@ export const createNewProduct = (product: string, callback: Function) => {
  * Gets all the products from the database.
  * @param callback callback method containing the result of the query.
  */
-export const getAllProducts = (callback: Function) => {
+export const getAllProducts = async (callback: Function) => {
     const query = "SELECT * FROM ak_products";
-    executePreparedQuery(query, callback);
+    
+    // executePreparedQuery(query, callback);
+}
+
+export const getProducts = (callback: Function)  => {
+    console.log("called");
+    
+    executeTransactions([
+        {
+            id: 1,
+            query: "SELECT * FROM ak_products",
+            parameters: []
+        }
+    ]).then(
+        val => {
+            callback(null, val[1].result)
+        }).catch(
+            err => callback(err)
+        );
 }
 
 /**
@@ -55,17 +76,106 @@ export const getProductByID = (productID:string, callback: Function) => {
 export const updateProductNameByID = (productID:string, newName:string, callback:Function) => {
     const queryOne = "UPDATE ak_products p SET p.name = ? WHERE p.id = ?;";
     const queryTwo = "UPDATE ak_products p SET p.name = ? WHERE p.name LIKE ?";
-    let query = "";
+    const queryThree = "SELECT * FROM ak_products p WHERE p.id = ?;";
+    const queryFour = "SELECT * FROM ak_products p WHERE p.name LIKE ?";
+    let queryToPerform = "";
+    let secondQuery = "";
     const numberID = parseInt(productID);
     if (isNaN(numberID)) {
-        query = queryTwo;
+        queryToPerform = queryTwo;
+        secondQuery = queryFour;
         productID = '%' + productID + '%';
     } else {
-        query = queryOne;
+        queryToPerform = queryOne;
+        secondQuery = queryThree;
     }
-    executePreparedQuery(query, callback, [newName, productID]);
+    // executePreparedQuery(query, callback, [newName, productID]);
+    executeTransactions([
+        {
+            id: 1,
+            query: queryToPerform,
+            parameters: [newName, productID]
+        },
+        {
+            id: 2,
+            query: secondQuery,
+            parameters: [productID]
+        }
+    ]).then(
+        val => {
+            const queryResults = val[1].result
+            if (queryResults.changedRows == 1) {
+                callback(null, val[2].result);
+            } else if (queryResults.affectedRows == 1) {
+                callback(new ItemAlreadyExistsError());
+            } else {
+                callback(new EmptySQLResultError('Was unable to find a match for the id.'))
+            }
+            
+        }).catch(
+            err => callback(err)
+        );
+}
+
+export const archiveProductByID = (productID:string, archive:string, callback:Function) => {
+    const queryOne = "UPDATE ak_products p SET p.archived = ? WHERE p.id = ?;";
+    const queryTwo = "UPDATE ak_products p SET p.archived = ? WHERE p.name LIKE ?;";
+    const queryThree = "SELECT * FROM ak_products p WHERE p.id = ?;";
+    const queryFour = "SELECT * FROM ak_products p WHERE p.name LIKE ?";
+    let queryToPerform = "";
+    let secondQuery = "";
+    const archiveNum = (archive =="true") ? 1 : 0;
+    const numberID = parseInt(productID);
+    if (isNaN(numberID)) {
+        queryToPerform = queryTwo;
+        secondQuery = queryFour;
+        productID = '%' + productID + '%';
+    } else {
+        queryToPerform = queryOne;
+        secondQuery = queryThree;
+    }
+    executeTransactions([
+        {
+            id: 1,
+            query: queryToPerform,
+            parameters: [archiveNum, productID]
+        },
+        {
+            id: 2,
+            query: secondQuery,
+            parameters: [productID]
+        }
+    ]).then(
+        val => {
+            const queryResults = val[1].result;
+            
+            if (queryResults.changedRows == 1) {
+                callback(null, val[2].result);
+            } else if (queryResults.affectedRows == 1) {
+                callback(new ItemAlreadyExistsError());
+            } else {
+                callback(new EmptySQLResultError('Was unable to find a match for the id.'));
+            }
+            
+        }).catch(
+            err => callback(err)
+        );
 }
 
 // 
 // ------------------------- Delete statements -------------------------
 // 
+
+export const deleteProductNameByID = (productId:string, callback:Function) => {
+    const queryOne = "DELETE FROM ak_products p WHERE p.id = ?;";
+    const queryTwo = "DELETE FROM ak_products p WHERE p.name LIKE ?";
+    let query = "";
+    const numberID = parseInt(productId);
+    if (isNaN(numberID)) {
+        query = queryTwo;
+        productId = '%' + productId + '%';
+    } else {
+        query = queryOne;
+    }
+    executePreparedQuery(query, callback, productId);
+}
--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -1,34 +1,13 @@
 import express from 'express';
 import { OkPacket, RowDataPacket } from 'mysql2';
 import { EmptySQLResultError } from '../exceptions/EmptySQLResultError';
-import { createNewProduct, getAllProducts, getProductByID, updateProductNameByID } from '../database/queries/productQueries';
-import { resolve } from 'path';
-import { ItemAlreadyExistsError } from '../exceptions/ItemAlreadyExistsError';
+import { archiveProductByID, createNewProduct, deleteProductNameByID, getAllProducts, getProductByID, getProducts, updateProductNameByID } from '../database/queries/productQueries';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    getAllProducts((err: Error, product: RowDataPacket[]) => {
-        if (err) {
-            next(err);
-        } else {
-            console.log(product);
-            res.status(200).json({ "products:": product })
-        }
-    });
-});
-
-router.get('/:productID', (req, res, next) => {
-    const productID = req.params.productID;
-    getProductByID(productID, (err: Error, product: RowDataPacket) => {
-        if (err) {
-            next(err)
-        } else {
-            console.log(product);
-            res.status(200).json({ "product:": product });
-        }
-    });
-});
+// 
+// ------------------------- Create endpoints -------------------------
+// 
 
 router.post('/', async (req, res, next) => {
     const name = req.body.name;
@@ -42,42 +21,82 @@ router.post('/', async (req, res, next) => {
     });
 });
 
+// 
+// ------------------------- Retrieve statements -------------------------
+// 
+
+router.get('/', (req, res, next) => {
+    getProducts((err: Error, product: RowDataPacket[]) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({ "products:": product })
+        }
+    });
+});
+
+router.get('/:productID', (req, res, next) => {
+    const productID = req.params.productID;
+    getProductByID(productID, (err: Error, product: RowDataPacket) => {
+        if (err) {
+            next(err)
+        } else {
+            res.status(200).json({ "product:": product });
+        }
+    });
+});
+
+// 
+// ------------------------- Update statements -------------------------
+// 
+
 router.post('/:productID', (req, res, next) => {
-    const id = req.body.id;
+    const id = req.params.productID;
     const name = req.body.name;
-    let updated = false;
 
-    const result = 
-        updateProductNameByID(id, name, (err: Error, product: OkPacket) => {
-            if (err) {
-                next(err);
+    updateProductNameByID(id, name, (err: Error, product: OkPacket) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({ "product:": product });
+        }
+    });
+
+});
+
+
+router.post('/:productID/archive', (req, res, next) => {
+    const id = req.params.productID;
+    const archive = req.body.archive;
+    archiveProductByID(id, archive, (err: Error, product: RowDataPacket) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({ "product:": product });
+        }
+    });
+});
+
+
+
+// 
+// ------------------------- Delete statements -------------------------
+// 
+
+router.post('/:productID/delete', (req, res, next) => {
+    const id = req.params.productID;
+    deleteProductNameByID(id, (err: Error, product: RowDataPacket) => {
+        if (err) {
+            next(err);
+        } else {
+            if (product.affectedRows == 1) {
+                res.status(200).json({ "product:": "The product has been deleted" });
             } else {
-                if (product.changedRows == 1) {
-                    console.log('Changed the product');
+                next(new EmptySQLResultError("No entry found for " + id));
+            }
 
-                    updated = true;
-                }
-            }
-            if(product.affectedRows == 1) {
-                next(new ItemAlreadyExistsError());
-            }
-            if (updated) {
-                getProductByID(id, async (err: Error, product: RowDataPacket) => {
-                    if (err) {
-                        next(err)
-                    } else {
-                        res.status(200).json({ "product:": product });
-                    }
-                });
-            
-            } else {
-                next(new EmptySQLResultError('Was unable to find a match for the id.'));
-            }
-        });
-    ;
-    
-    
-
+        }
+    });
 });
 
 export default router;
