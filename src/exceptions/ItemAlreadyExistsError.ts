051bc0f823fa8566fb4462f2583be2b2027cc4e7
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-29 16:26:11 +0100

    Updated error message

--- src/exceptions/ItemAlreadyExistsError.ts
+++ src/exceptions/ItemAlreadyExistsError.ts
@@ -1,7 +1,7 @@
 export class ItemAlreadyExistsError extends Error {
     public status: number;
     constructor() {
-        super('Item already exists in the database.');
+        super('Item already exists in the database or no value was changed.');
         this.status = 403;
 
         // Set the prototype explicitly.
