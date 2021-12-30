6a8c425a42db1e91daa5dc27fcec384f1de65504
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2021-12-30 12:39:37 +0100

    Added method to convert datestring to sql format

---
+++ src/util/ConvertStringToSQLDate.ts
@@ -0,0 +1,13 @@
+/**
+ * This method converts the string it receives into a format that 
+ * can be inserted into the MySQL database.
+ * @param date string containing the date format.
+ * @returns the string formatted for MySQL support.
+ */
+export function convertStringToSQLDate(date:string) {
+    console.log(date);
+    
+    const dateOfBirth = new Date(date).toISOString();
+    const isoDate = new Date(dateOfBirth);
+    return isoDate.toJSON().slice(0, 19).replace('T', ' ');
+}
