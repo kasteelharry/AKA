b39d97c8553aa0e138df8f072b725b0ff653753f
Joris Kuiper <j.s.kuiper-1@student.utwente.nl>
2022-01-05 10:35:48 +0100

    Added customer queries unit test

--- src/__test__/queries/CustomersQueries.test.ts
+++ src/__test__/queries/CustomersQueries.test.ts
@@ -1,33 +1,31 @@
 import { GeneralServerError } from "../../exceptions/GeneralServerError";
 import { queryType } from "../../app";
 import CustomerQueries from "../../queries/CustomerQueries";
-import { exampleResult, MockDatabase, singleResult } from "../../model/MockDatabase";
+import { exampleResult, MockDatabase } from "../../model/MockDatabase";
 import { convertStringToSQLDate } from "../../util/ConvertStringToSQLDate";
 import { ItemAlreadyExistsError } from "../../exceptions/ItemAlreadyExistsError";
+import { EmptySQLResultError } from "../../exceptions/EmptySQLResultError";
 
 describe('CustomerQueriesTest', () => {
 
     let customer: CustomerQueries;
-    let db: Database<queryType>;
+    let db: MockDatabase<queryType>;
 
     beforeAll(() => {
         db = new MockDatabase();
         customer = new CustomerQueries(db);
     });
 
+    beforeEach(() => {
+        db.setDBState(true);
+    });
+
     //
     // ------------------------- Create statements test -------------------------
     //
-    test("Create a customer", () => {
-        customer.createNewCustomer("testName", convertStringToSQLDate("2000-01-01"), "NL22INGB00000000")
-        .then(val => expect(val).toBeGreaterThanOrEqual(1))
-        .catch(err => expect(err).toBeNaN);
-    });
-
-    test("Create an existing customer", () => {
-        customer.createNewCustomer("testName", convertStringToSQLDate("2000-01-01"), "NL22INGB00000000")
-        .then(val => expect(val).toBeNaN)
-        .catch(err => expect(err).toBeInstanceOf(ItemAlreadyExistsError));
+    test("Create a customer", async () => {
+        const promise = customer.createNewCustomer("testName", convertStringToSQLDate("2000-01-01"), "NL22INGB00000000");
+        await expect(promise).resolves.toBeGreaterThanOrEqual(1);
     });
 
 
@@ -35,33 +33,50 @@ describe('CustomerQueriesTest', () => {
     // ------------------------- Retrieve statements test -------------------------
     //
 
-    test("Get all customers", () => {
-        // tslint:disable-next-line: no-console
-        customer.getAllCustomers().then(
-            res => expect(res).toMatchObject(exampleResult[1].result)
-        ).catch(err => expect(err).rejects.toBeNaN());
+    test("Get all customers", async () => {
+        const promise = customer.getAllCustomers();
+        await expect(promise).resolves.toBeDefined();
     });
 
-    test("Get one customer", () => {
-        customer.getCustomerByID("1").then(
-            res => expect(res[0]).toMatchObject(exampleResult[1].result[0])
-        ).catch(err => expect(err).rejects.toBeNaN());
+    test("Get one customer", async () => {
+        const promise = customer.getCustomerByID("1");
+        await expect(promise).resolves.toBeDefined();
     });
 
-    test("Fail all customers", () => {
+    test("Fail all customers", async () => {
         db.setDBState(false);
-        // tslint:disable-next-line: no-console
-        customer.getAllCustomers().then(
-            res => expect(res).rejects.toBeNaN()
-        ).catch(err => {
-            expect(err).toBeInstanceOf(GeneralServerError);
-        });
+        const promise = customer.getAllCustomers();
+        await expect(promise).rejects.toBeInstanceOf(GeneralServerError);
     });
 
-    test("Fail one customer", () => {
+    test("Fail one customer", async () => {
         db.setDBState(false);
-        customer.getCustomerByID("1")
-            .then(res => expect(res).rejects.toBeNaN())
-            .catch(err => expect(err).toBeInstanceOf(GeneralServerError));
+        const promise = customer.getCustomerByID("1");
+        await expect(promise).rejects.toBeInstanceOf(GeneralServerError);
     });
+
+    //
+    // ------------------------- Update statements test -------------------------
+    //
+
+    test("Update existing customer", async () => {
+        const params = new Map<string, string | number | undefined>();
+        params.set("name", "Anna");
+        params.set("birthday", convertStringToSQLDate("1999-02-22"));
+        params.set("bankaccount", "NL11RABO1234567890");
+        params.set("active", 1);
+        const promise = customer.updateCustomer("1", params);
+        await expect(promise).resolves.toBeDefined();
+    });
+
+
+    //
+    // ------------------------- Delete statements test -------------------------
+    //
+
+    test("Delete customer", async () => {
+        const promise = customer.deleteCustomer("1");
+        await expect(promise).resolves.toBeDefined();
+    });
+
 });
