5db6c6cfab6ae544339524d19ac93285867afdec
Joris Kuiper <joriskuiper2@gmail.com>
2022-05-08 18:41:04 +0200

    Added new components + new menu to main container

--- client/src/App/AppContainer.tsx
+++ client/src/App/AppContainer.tsx
@@ -1,32 +1,167 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet'
 
 import './AppContainer.scss';
 import LoginPage from './Login/LoginPage';
-import Menu from './Menu/Menu';
+import MenuBar from './Menu/MenuBar';
 import AppBody from './AppBody/AppBody';
 import RegisterPage from './Register/RegisterPage';
+import TransactionPage from './Transaction/TransactionPage';
+import { Navigate } from 'react-router-dom';
+import SelectionScreen from './SelectionScreen/SelectionScreen';
+import { ThemeProvider } from '@mui/material/styles';
+import { AsterionTheme } from '../theme/AsterionTheme.d';
+import EventScreen from './EventScreen/EventScreen';
+
+const localStorageWhiteList = ['loggedIn', 'activeEvent'];
 
 function AppContainer() {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="AppContainer">
-      <Helmet>
-        <title>{t('app_title')}</title>
-      </Helmet>
+    // Set the logged in state. Although this is also kept track of on the server side,
+    // some extra checks cannot hurt.
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
-      {/* React Router configures the Components that need to be rendered when certain paths are requested. */}
-      <Menu />
-      <Routes>
-        <Route path="/login/*" element={<LoginPage />} />
-        <Route path="/register/*" element={<RegisterPage />} />
-        <Route path="/" element={<AppBody />} />
-      </Routes>
-    </div>
-  );
+    const [activeEvent, setActiveEvent] = useState<number>(-1)
+
+    // Set the flag for the event that this is the first render. Then we do not want to
+    // run the use effect trigger.
+    const firstUpdate = useRef(true);
+
+    /**
+     * If the logged in status is changed to false, then log the user out on the server.
+     */
+    useEffect(() => {
+        if (firstUpdate.current) {
+            firstUpdate.current = true;
+        } else {
+            if (!loggedIn) {
+                fetch('http://localhost:8080/logout', { credentials: 'include' })
+                    .then(response => response.json())
+                    .then(data => console.log(data));
+            }
+        }
+
+    }, [loggedIn]);
+
+    useEffect(() => {
+        console.log("Current active event is: ", activeEvent);
+        clearHistory();
+        const savedEvent= localStorage.getItem('activeEvent');
+        if (savedEvent !== null && !isNaN(parseInt(savedEvent))) {
+            setActiveEvent(parseInt(savedEvent))
+            localStorage.setItem('activeEvent', savedEvent.toString())
+        } else if (activeEvent !== -1) {
+            localStorage.setItem('activeEvent', activeEvent.toString());
+        }
+    }, [activeEvent])
+
+    /**
+     * This function calls the login hook and sets its state.
+     * @param status the new login status
+     */
+    function setLoginHook(status: boolean) {
+        setLoggedIn(status);
+    }
+
+    /**
+     * Checks if the user is logged in, if not it forces the user to log in.
+     * @param param0 The children components.
+     * @returns Either the child component or it navigates the user to the login page.
+     */
+    const PrivateRoute = ({ children }: any) => {
+        return loggedIn || localStorage.getItem('loggedIn') === 'true' ? children : <Navigate to="/login" />;
+    }
+
+    /**
+     * Ensures that the user is logged out. This is a JSX component
+     * such that it can be called from the element in the router.
+     * @returns The user to the login page.
+     */
+    const SetLogout = () => {
+        setLoginHook(false);
+        fetch('http://localhost:8080/logout', { credentials: 'include' })
+            .then(response => response.json())
+            .then(data => console.log(data));
+        localStorage.setItem('loggedIn', 'false');
+        return <Navigate to='/login' />;
+    }
+
+    return (
+        <ThemeProvider theme={AsterionTheme}>
+            <div className="AppContainer">
+                <Helmet>
+                    <title>{t('app_title')}</title>
+                </Helmet>
+
+                {/* React Router configures the Components that need to be rendered when certain paths are requested. */}
+                <MenuBar logout={setLoggedIn} />
+                <Routes>
+                    <Route path="/login/*" element={loggedIn ? <Navigate to='/selection' /> : <LoginPage loggedIn={loggedIn} setLoggedIn={setLoginHook} />
+                    } />
+                    <Route path='/logout/*' element={<SetLogout />} />
+                    <Route path="/register/*" element={<RegisterPage />} />
+                    <Route path="/" element={<AppBody />} />
+                    <Route path='/transaction/:customerID' element={
+                        <PrivateRoute>
+                            <TransactionPage activeEvent={activeEvent} />
+                        </PrivateRoute>} />
+                    {/* <Route path='/selection/*' element={<SelectionScreen/>}/> */}
+                    {
+                    activeEvent !== -1 && !isNaN(activeEvent) &&
+                        <Route
+                        path='/selection/*'
+                        element={
+                            <PrivateRoute>
+                                <SelectionScreen
+                                activeEvent={activeEvent}
+                                // manyActiveEvents={manyActiveEvents}
+                                // setActiveEvent={setActiveEvent}
+                                />
+                            </PrivateRoute>
+
+                        } />
+                    }
+                    {
+                    (activeEvent === -1 || isNaN(activeEvent)) &&
+                    <Route path='/selection/*' element={
+                        <PrivateRoute>
+                            <EventScreen activeEvent={activeEvent} setActiveEvent={setActiveEvent}/>
+                        </PrivateRoute>} />
+                    }
+                    <Route path='/event' element={
+                        <EventScreen activeEvent={activeEvent} setActiveEvent={setActiveEvent}/>
+                    }/>
+                    
+                </Routes>
+            </div>
+        </ThemeProvider >
+
+    );
+}
+/**
+ * Ensures that the user is logged out.
+ * @returns The user to the login page.
+ */
+export const SetLogout = () => {
+    const navigate = useNavigate();
+    localStorage.clear();
+    fetch('http://localhost:8080/logout', { credentials: 'include' })
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+    return navigate('/login');
+}
+
+export const clearHistory = () => {
+    const keys = Object.keys(localStorage);
+    keys.forEach(key => {
+        if (!localStorageWhiteList.includes(key)) {
+            localStorage.removeItem(key);
+        }
+    })
 }
 
 export default AppContainer;
---
+++ client/src/App/Menu/MenuBar.tsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom';
+import LanguageSelector from './LanguageSelector';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import './Menu.scss';
+
+
+
+
+
+function MenuBar(props:any) {
+    const { t } = useTranslation();
+    const pages = [t('nav.dashboard'), t('nav.transaction'), t('nav.statistics')];
+    const pagesPath = ['/dashboard', '/selection', '/statistics'];
+    const settings = [t('user.profile'), t('user.account'), t('user.dashboard'), t('user.logout')];
+    const settingsPath = ['/profile', '/account', '/dashboard', '/logout'];
+
+    const navigate = useNavigate()
+
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
+  
+
+    return (
+        <AppBar position="static">
+        <Container maxWidth={false}>
+          <Toolbar disableGutters>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              {t('app_title')}
+            </Typography>
+  
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              {t('app_title')}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
+              {pages.map((page, index) => (
+                <Button
+                  key={page}
+                  onClick={() => {
+                      handleCloseNavMenu();
+                      navigate(pagesPath[index]);
+                      }}
+                  sx={{ fontSize:18, my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            <Box>
+            <LanguageSelector />
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar sx={{ fontSize:25, height: '60px', width: '60px' }}>SB</Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{  mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting, index) => (
+                  <MenuItem key={setting} onClick={() => {
+                      handleCloseUserMenu();
+                      navigate(settingsPath[index]);
+                  }
+                    }>
+                    <Typography sx={{fontSize:20}} textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    )
+}
+
+export default MenuBar
